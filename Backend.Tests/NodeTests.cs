728a06628cb7fb387f18a39a6ff1e6a0ca48f8fa
Nnaemeka Nnadede <nn454994@dal.ca>
2025-11-04 11:02:34 -0400

    Added a test to check  node ability to link left and right

--- Backend.Tests/NodeTests.cs
+++ Backend.Tests/NodeTests.cs
@@ -3,17 +3,34 @@
 public class NodeTests
 {
     [Fact]
-        public void Constructor_ShouldInitializeValueAndNullChildren()
-        {
-            // Arrange
-            char expected = 'A';
+    public void Constructor_ShouldInitializeValueAndNullChildren()
+    {
+        // Arrange
+        char expected = 'A';
 
-            // Act
-            var node = new Node(expected);
+        // Act
+        var node = new Node(expected);
 
-            // Assert
-            Assert.Equal(expected, node.Value);
-            Assert.Null(node.Left);
-            Assert.Null(node.Right);
-        }
+        // Assert
+        Assert.Equal(expected, node.Value);
+        Assert.Null(node.Left);
+        Assert.Null(node.Right);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToLinkLeftAndRightNodes()
+    {
+        // Arrange
+        var parent = new Node('P');
+        var left = new Node('L');
+        var right = new Node('R');
+
+        // Act
+        parent.Left = left;
+        parent.Right = right;
+
+        // Assert
+        Assert.Same(left, parent.Left);
+        Assert.Same(right, parent.Right);
+    }
 }
