1e18f4fc69846fba98b0c16f810fa3b804594bf4
Nnaemeka Nnadede <nn454994@dal.ca>
2025-11-03 23:12:27 -0400

    Add initial test project and NodeTests class for unit testing

---
+++ Backend.Tests/NodeTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Tests;
+
+public class NodeTests
+{
+    [Fact]
+        public void Constructor_ShouldInitializeValueAndNullChildren()
+        {
+            // Arrange
+            char expected = 'A';
+
+            // Act
+            var node = new Node(expected);
+
+            // Assert
+            Assert.Equal(expected, node.Value);
+            Assert.Null(node.Left);
+            Assert.Null(node.Right);
+        }
+}
