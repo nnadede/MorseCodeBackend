b52a855f0f5b16f06e2c31568257f97f3e3f6486
Nnaemeka Nnadede <nn454994@dal.ca>
2025-11-06 20:53:31 -0400

    Initialize BinaryTree and add console output for root status

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Backend;
+
+BinaryTree tree = new BinaryTree();
+//tree.InitTree();
+
+if (tree.Root == null)
+{
+    Console.WriteLine("Tree is not initialized.");
+}else
+{
+    Console.WriteLine("Tree is initialized.");
+}
+
 Console.WriteLine("Hello, World!");
+// Console.WriteLine($"Root Node Value: {tree.Root?.Left?.Value}"); // Example to show usage
+
+
